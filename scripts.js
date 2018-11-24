2f100e40af4967dfa771289dbe7435ae8737af83
Adam <flashbackditto@users.noreply.github.com>
2018-11-23 23:30:33 -0800

    JS to access API, displaying info via DOM manipulation

--- scripts.js
+++ scripts.js
@@ -20,7 +20,27 @@ var data = JSON.parse(this.response);
 
 if (request.status >= 200 && request.status < 400) { //optional if statement to console log if theres a 404 not found error or otherwise
   data.forEach( country => {
-    console.log(country.name && country.population);
+
+    //Creates a div with a class of card
+    const card = document.createElement('div');
+    card.setAttribute('class', 'card');
+
+
+    //creates an h1 set to the Countrys Name
+    const h1 = document.createElement('h1');
+    h1.textContent = country.name;
+
+    //creates a paragraph and seets the text to population
+    const p = document.createElement('p');
+    country.population = country.population;
+    p.textContent = `Population:${country.population}`;
+
+    //appends the cards to the container element
+    container.appendChild(card);
+
+    //each card will have an h1 and p
+    card.appendChild(h1);
+    card.appendChild(p);
   });
  } else {
   console.log('error');
@@ -28,3 +48,15 @@ if (request.status >= 200 && request.status < 400) { //optional if statement to 
 }
 
 request.send();
+
+const app = document.getElementById('root');
+
+const logo = document.createElement('img');
+
+logo.src = 'img/logo.png';
+
+const container = document.createElement('div');
+container.setAttribute('class','container');
+
+app.appendChild(logo);
+app.appendChild(container);
