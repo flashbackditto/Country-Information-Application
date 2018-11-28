d84fa00399583b16539a6bfb2913388c41455907
Adam <flashbackditto@users.noreply.github.com>
2018-11-27 19:12:00 -0800

    Added sort by Pop and Alphabetized btns functionality

--- scripts.js
+++ scripts.js
@@ -90,6 +90,61 @@ document.getElementById("clickMe").onclick  = function () {
 
 var data = JSON.parse(request.response);
 
+
+if (request.status >= 200 && request.status < 400) {  //optional if statement to console log if theres a 404 not found error or otherwise
+
+  var byPop = data.slice(0);
+  byPop.sort(function(a,b) {
+      return a.population - b.population;
+  });
+
+
+  byPop.forEach( country => {
+
+    //Creates a div with a class of card
+    const card = document.createElement('div');
+    card.setAttribute('class', 'card');
+
+
+    //creates an h1 set to the Countrys Name
+    const h1 = document.createElement('h1');
+    h1.textContent = country.name;
+
+    //creates a paragraph and seets the text to population
+
+    const p = document.createElement('p');
+    country.population = country.population;
+    p.textContent = `Population:${country.population}`;
+
+    //appends the cards to the container element
+    container.appendChild(card);
+
+    //each card will have an h1 and p
+    card.appendChild(h1);
+    card.appendChild(p);
+  });
+ } else {
+  console.log('error');
+}
+
+}
+
+request.send();
+
+
+// creating a request variable and assigning a new XMLRequest object to it
+var request = new XMLHttpRequest ();
+
+
+//Open a new connection, using GET request on the URL endpoint
+request.open('GET', 'https://restcountries.eu/rest/v2/all', true);
+
+
+document.getElementById('alphabetical').onclick = function () { //default data structure is Alphabetical
+
+//begin accessing JSON data here
+var data = JSON.parse(request.response);
+
 if (request.status >= 200 && request.status < 400) { //optional if statement to console log if theres a 404 not found error or otherwise
   data.forEach( country => {
 
@@ -116,8 +171,7 @@ if (request.status >= 200 && request.status < 400) { //optional if statement to 
   });
  } else {
   console.log('error');
-}
-
+ }
 }
 
 request.send();
