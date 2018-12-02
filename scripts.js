b30c31f1b2b77569ffcf26936059f2abc1ccf3a0
Adam <flashbackditto@users.noreply.github.com>
2018-12-01 23:50:14 -0800

    Added sort by kilometer size, need to remove first dom element

--- scripts.js
+++ scripts.js
@@ -134,7 +134,7 @@ if (request.status >= 200 && request.status < 400) {  //optional if statement to
     const h3 = document.createElement('h3');
 
     if (country.capital === "") {
-        h3.textContent = "No Capital Info Available";
+        h3.textContent = "No Capital";
     }else {
         h3.textContent = `Capital: ${country.capital}`;
    }
@@ -142,7 +142,7 @@ if (request.status >= 200 && request.status < 400) {  //optional if statement to
     //creates a paragraph and seets the text to population
 
     const p = document.createElement('p');
-    country.population = country.population;
+    country.population = country.population.toString().replace(/\B(?=(\d{3})+(?!\d))/g, ",");
     p.textContent = `Population: ${country.population}`;
 
     const p2 = document.createElement('p');
@@ -171,6 +171,109 @@ if (request.status >= 200 && request.status < 400) {  //optional if statement to
 
 request.send();
 
+
+// creating a request variable and assigning a new XMLRequest object to it
+var request = new XMLHttpRequest ();
+
+
+//Open a new connection, using GET request on the URL endpoint
+request.open('GET', 'https://restcountries.eu/rest/v2/all', true);
+// var data = JSON.parse(this.response);
+
+
+var sizeButton = document.getElementById("size").addEventListener('click', () => {
+  removeLast();
+  areaGetThenSort();
+});
+
+
+function areaGetThenSort (e) {
+var data = JSON.parse(request.response);
+
+if (request.status >= 200 && request.status < 400) {  //optional if statement to console log if theres a 404 not found error or otherwise
+
+  var byArea = data.slice(0);
+  byArea.sort(function(a,b) {
+      return a.area - b.area;
+  });
+
+
+  byArea.forEach( country => {
+
+    //re-Creates Container thats removed by popButtonClick function
+    const container = document.createElement('div');
+    container.setAttribute('class','container');
+
+    //Creates a div with a class of card
+    const card = document.createElement('div');
+    card.setAttribute('class', 'card');
+
+
+    //creates an h1 set to the Countrys Name
+    const h1 = document.createElement('h1');
+    h1.textContent = country.name;
+
+    //Creates an h3 set to capital Name
+    const h3 = document.createElement('h3');
+
+    if (country.capital === "") {
+        h3.textContent = "No Capital";
+    }else {
+        h3.textContent = `Capital: ${country.capital}`;
+   }
+
+    //creates a paragraph and seets the text to population
+
+    const p = document.createElement('p');
+    country.population = country.population.toString().replace(/\B(?=(\d{3})+(?!\d))/g, ",");
+    p.textContent = `Population: ${country.population}`;
+
+    const p2 = document.createElement('p');
+    p2.textContent = `Currency: ${country.currencies[0].name}`;
+
+    const p3 = document.createElement('p');
+
+
+   //START OF SIZE SORT ISSUES
+
+
+    if (country.area == null) {
+    var countryNode = document.getElementById("root");
+      while (countryNode.hasChildNodes()) {
+      countryNode.removeChild(countryNode.lastChild);
+     // for (let i = countryNode.childNodes.length - 1; i >= 0; i--) {
+     //    countryNode.removeChild(countryNode.childNodes[i]);
+     }
+
+   } else {
+      country.area = country.area.toString().replace(/\B(?=(\d{3})+(?!\d))/g, ",");
+    p3.textContent = `Size: ${country.area} sq kilometers`;
+  }
+
+
+    //re-appends container after removal
+    app.appendChild(container);
+
+    //appends the cards to the container element
+    container.appendChild(card);
+
+    //each card will have an h1 and p
+    card.appendChild(h1);
+    card.appendChild(h3);
+    card.appendChild(p);
+    card.appendChild(p2);
+    card.appendChild(p3);
+
+
+
+  });
+ } else {
+  console.log('error');
+}
+
+}
+
+request.send();
 
 // creating a request variable and assigning a new XMLRequest object to it
 var request = new XMLHttpRequest ();
@@ -215,16 +318,16 @@ if (request.status >= 200 && request.status < 400) { //optional if statement to 
     const h3 = document.createElement('h3');
 
  if (country.capital === "") {
-     h3.textContent = "No Capital Info Available";
+     h3.textContent = "No Capital";
  }else {
      h3.textContent = `Capital: ${country.capital}`;
 }
 
     //creates a paragraph and seets the text to population
     const p = document.createElement('p');
-    
+    country.population = country.population.toString().replace(/\B(?=(\d{3})+(?!\d))/g, ",");
     p.textContent = `Population: ${country.population}`;
-
+    console.log(typeof country.population)
     const p2 = document.createElement('p');
     p2.textContent = `Currency: ${country.currencies[0].name}`;
 
@@ -249,7 +352,7 @@ request.send();
 
 //Problem to solve
 //each time a button is clicked it adds the DOM elements to the page without refreshing
-//the page and clearing away the previously creted DOM elements  SOLVED..? I think, needs a few tests to be done
+//the page and clearing away the previously creted DOM elements  SOLVED!
 
 //got Population button to clear page, but unable to present new DOM elements, perhaps I need
 //to change to event listeners, bubbling problem perhaps..? SOLVED!!
@@ -258,5 +361,6 @@ request.send();
 // and re-appendChild to app.
 
 //Next step is apply the fix to Alphabetical sort SOLVED!
-// run tests, create third sorting function with new approach
+// run tests, create third sorting function with new approach **SIZE SORT***NEEDS WORK
+
 // and add a second api with functionality
