e2877fa482e313b54c7764aa217c733f0df26948
flashbackditto <chaunceyberkowits@yahoo.com>
2019-04-24 17:22:49 -0700

    cleared out old/redundant comments

--- scripts.js
+++ scripts.js
@@ -1,67 +1,3 @@
-// XMLHttpRequest is a widely supported built-in method of making requests.
-// There is also a newer Fetch API which can be used for the same purpose,
-// which is simpler but has less browser support.
-
-
-
-// const app = document.getElementById('root');
-// const world = document.getElementById('top');
-// const logo = document.createElement('img');
-//
-// logo.src = 'img/logo.png';
-//
-//
-// const container = document.createElement('div');
-// container.setAttribute('class','container');
-//
-// world.appendChild(logo);
-// app.appendChild(container);
-//
-// // creating a request variable and assigning a new XMLRequest object to it
-// var request = new XMLHttpRequest ();
-//
-//
-// //Open a new connection, using GET request on the URL endpoint
-// request.open('GET', 'https://restcountries.eu/rest/v2/all', true);
-// // var data = JSON.parse(this.response);
-//
-//
-// request.onload = function () {
-//
-// //begin accessing JSON data here
-// var data = JSON.parse(this.response);
-//
-// if (request.status >= 200 && request.status < 400) { //optional if statement to console log if theres a 404 not found error or otherwise
-//   data.forEach( country => {
-//
-//     //Creates a div with a class of card
-//     const card = document.createElement('div');
-//     card.setAttribute('class', 'card');
-//
-//
-//     //creates an h1 set to the Countrys Name
-//     const h1 = document.createElement('h1');
-//     h1.textContent = country.name;
-//
-//     //creates a paragraph and seets the text to population
-//     const p = document.createElement('p');
-//     country.population = country.population;
-//     p.textContent = `Population:${country.population}`;
-//
-//     //appends the cards to the container element
-//     container.appendChild(card);
-//
-//     //each card will have an h1 and p
-//     card.appendChild(h1);
-//     card.appendChild(p);
-//   });
-//  } else {
-//   console.log('error');
-//  }
-// }
-//
-// request.send();
-
 
 const app = document.getElementById('root');
 const world = document.getElementById('top');
@@ -115,7 +51,7 @@ if (request.status >= 200 && request.status < 400) {  //optional if statement to
     const card = document.createElement('div');
     card.setAttribute('class', 'card');
 
-   //Makes card a link to reeant wiki page
+   //Makes card a link to relevant wiki page
     const a = document.createElement('a');
     a.setAttribute('target', '_"blank"');
     a.setAttribute('href', `https://en.wikipedia.org/wiki/${country.name}`);
@@ -443,22 +379,16 @@ const searchBar = document.forms['searchCountries'].querySelector('input');
 searchBar.addEventListener('keyup', function(e){
   removeLast();
 
-
 //begin accessing JSON data here
 var data = JSON.parse(request.response);
 
 if (request.status >= 200 && request.status < 400) { //optional if statement to console log if theres a 404 not found error or otherwise
 
-
-
   data.forEach( country => {
-
-
 
     //creates an h1 set to the Countrys Name
     const h1 = document.createElement('h1');
     h1.textContent = country.name;
-
 
     //re-Creates Container thats removed by popButtonClick function
     const container = document.createElement('div');
@@ -474,7 +404,6 @@ if (request.status >= 200 && request.status < 400) { //optional if statement to 
        container.appendChild(a);
        a.appendChild(card);
 
-
     //Creates an h3 set to capital Name
     const h3 = document.createElement('h3');
 
@@ -488,7 +417,6 @@ if (request.status >= 200 && request.status < 400) { //optional if statement to 
     const p = document.createElement('p');
     country.population = country.population.toString().replace(/\B(?=(\d{3})+(?!\d))/g, ",");
     p.textContent = `Population: ${country.population}`;
-
 
     const p2 = document.createElement('p');
     p2.textContent = `Currency: ${country.currencies[0].name}`;
@@ -526,7 +454,6 @@ if (request.status >= 200 && request.status < 400) { //optional if statement to 
       p4.textContent = `Temperature at Capital: ${weatherData.main.temp}F`;
       p5.textContent = `Weather Conditions: ${weatherData.weather[0].description}`;
     }
-
    }
 
       weatherRequest.send ();
@@ -564,20 +491,3 @@ if (request.status >= 200 && request.status < 400) { //optional if statement to 
 });
 
 request.send();
-
-
-
-//Problem to solve
-//each time a button is clicked it adds the DOM elements to the page without refreshing
-//the page and clearing away the previously creted DOM elements  SOLVED!
-
-//got Population button to clear page, but unable to present new DOM elements, perhaps I need
-//to change to event listeners, bubbling problem perhaps..? SOLVED!!
-
-// Solved removal on click, example is in the popButton button funtions, just had to re-createElement after removal
-// and re-appendChild to app.
-
-//Next step is apply the fix to Alphabetical sort SOLVED!
-// run tests, create third sorting function with new approach **SIZE SORT***NEEDS WORK
-
-// and add a second api with functionality
